a96eda9c49ed924d52749b98776e3944f95e17a9
CyberWol-12 <divyau0802@gmail.com>
2026-05-30 15:52:41 +0530

    http requests

--- trulia webscraping.py
+++ trulia webscraping.py
@@ -1,1 +1,9 @@
-print('Hello World')
+# import libraries
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+
+website = 'https://www.trulia.com/CA/San_Diego/'
+
+response = requests.get(website)
