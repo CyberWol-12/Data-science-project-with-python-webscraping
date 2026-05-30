507370d55abba234e13a51f641936c87bb57192f
CyberWol-12 <divyau0802@gmail.com>
2026-05-30 17:41:39 +0530

    http requests

--- trulia webscraping.py
+++ trulia webscraping.py
@@ -1,9 +1,15 @@
-# import libraries
+#import libraries
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
+#store website in variable
+website = 'https://www.rentcafe.com/apartments-for-rent/san-diego-ca/'
 
-website = 'https://www.trulia.com/CA/San_Diego/'
-
-response = requests.get(website)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"
+}
+#Get requests
+response = requests.get(website,headers=headers)
+#Status Code
+print(response.status_code)
