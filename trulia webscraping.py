22739f598e1c9bc5eea598537aa7f0c6ae20a322
CyberWol-12 <divyau0802@gmail.com>
2026-05-31 11:56:39 +0530

    find require element

--- trulia webscraping.py
+++ trulia webscraping.py
@@ -3,13 +3,38 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
+result_update = []
+
+
+#baseurl
+baseurl = 'https://www.hostelworld.com/'
+
 #store website in variable
-website = 'https://www.rentcafe.com/apartments-for-rent/san-diego-ca/'
+website = 'https://www.hostelworld.com/pwa/s?q=San%20Diego,%20California,%20USA&country=California,%20USA&city=San%20Diego&type=city&id=129&from=2026-06-01&to=2026-06-04&guests=2&page=1'
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"
 }
 #Get requests
-response = requests.get(website,headers=headers)
+r = requests.get(website,headers=headers)
 #Status Code
-print(response.status_code)
+r.status_code
+
+soup = BeautifulSoup(r.content,'html.parser')
+
+#Results
+result_container = soup.find_all('div',class_ = "property-card")
+#we just want to target the element which have the attribute
+for r in result_container:
+    if r.has_attr('data-v-64e39c4e'):
+        result_update.append(r)
+print(len(result_update))
+
+  
+
+
+    
+
+
+
+
